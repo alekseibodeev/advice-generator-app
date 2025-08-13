cdd96bfc6924d29e338c1ca5f4d9d00ade8de8a3
Aleksei Bodeev <bodeevaleksei@gmail.com>
2025-08-13 12:47:41 +0500

    Add fetching animation

--- script.js
+++ script.js
@@ -1,22 +1,40 @@
 (function () {
     const RANDOM_ADVISE_URL = "https://api.adviceslip.com/advice";
+    const DICE_FETCH_ANIMATION = "rotate-infinite";
 
     const adviceIdDisplay = document.getElementById("advice-id");
     const adviceContentDisplay = document.getElementById("advice-content");
     const adviceRollButton = document.getElementById("advice-roll");
+    const adviceDiceIcon = adviceRollButton.querySelector("img");
+
+    let isFetchingAdvice = false;
 
     adviceRollButton.addEventListener("click", rollClickHandler);
+    adviceDiceIcon.addEventListener("animationiteration", animationHandler);
 
     async function rollClickHandler() {
-        const { id, advice } = await getRandomAdvice();
+        if (!isFetchingAdvice) {
+            isFetchingAdvice = true;
+            adviceDiceIcon.classList.toggle(DICE_FETCH_ANIMATION);
 
-        adviceIdDisplay.textContent = id;
-        adviceContentDisplay.textContent = advice;
+            const { id, advice } = await getRandomAdvice();
+
+            adviceIdDisplay.textContent = id;
+            adviceContentDisplay.textContent = advice;
+
+            isFetchingAdvice = false;
+        }
     }
 
     async function getRandomAdvice() {
         const res = await fetch(RANDOM_ADVISE_URL);
         const { slip } = await res.json();
         return slip;
+    }
+
+    function animationHandler() {
+        if (!isFetchingAdvice) {
+            adviceDiceIcon.classList.toggle(DICE_FETCH_ANIMATION);
+        }
     }
 })();
